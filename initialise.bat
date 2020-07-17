8d2a1714750ee8d2059c2559650017685e98a47a
skmrSharma <54986978+skmrSharma@users.noreply.github.com>
2020-07-17 13:14:32 +0530

    Update initialise.bat

--- initialise.bat
+++ initialise.bat
@@ -1,5 +1,5 @@
 echo image_file_list = [ > image_file_list.json
 setlocal enabledelayedexpansion
 :: replace the . following 'where' with the path of the folder where images are
-for /f "tokens=*" %%x in ('where "E:\j:*.jpg"') do (set "filepath=%%x" & set "filepath=file:///!filepath:\=/!" & echo "!filepath!", >> image_file_list.json) 
+for /f "tokens=*" %%x in ('where "*.jpg"') do (set "filepath=%%x" & set "filepath=file:///!filepath:\=/!" & echo "!filepath!", >> image_file_list.json) 
 echo ] >> image_file_list.json
