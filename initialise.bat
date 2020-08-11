7fd84033960b76169231b04e3db078ef50f3d2a7
skmrSharma <54986978+skmrSharma@users.noreply.github.com>
2020-08-11 12:59:11 +0530

    easier editing

--- initialise.bat
+++ initialise.bat
@@ -1,5 +1,6 @@
 echo image_file_list = [ > image_file_list.json
+:: Replace %cd% with path of the folder containing images, %cd% for current folder
+set imagePath=%cd%
 setlocal enabledelayedexpansion
-:: replace the . following 'where' with the path of the folder where images are
-for /f "tokens=*" %%x in ('where "*.jpg"') do (set "filepath=%%x" & set "filepath=file:///!filepath:\=/!" & echo "!filepath!", >> image_file_list.json) 
+for /f "tokens=*" %%g in ('where "%imagePath%:*.jpg" "%imagePath%:*.png"') do (set "filepath=%%g" & set "filepath=file:///!filepath:\=/!" & echo "!filepath!", >> image_file_list.json)
 echo ] >> image_file_list.json
